60e1be3129305d732f7f32cdd0ef7f6c37bf5e4d
Aredruss <aredruss@gmail.com>
2019-11-23 15:46:44 +0300

    Minor refactoring

--- app/src/test/java/com/redbox/mirumon/test/GroupListTest.kt
+++ app/src/test/java/com/redbox/mirumon/test/GroupListTest.kt
@@ -11,7 +11,7 @@ import org.junit.runners.JUnit4
 @RunWith(JUnit4::class)
 class GroupListTest {
 
-    lateinit var adapter: GroupListAdapter
+    private lateinit var adapter: GroupListAdapter
 
     @Before
     fun setAddress() {
